25906a14749051925dbde4d59a973a09a78caf75
Penelope Jungreis <penelope.jungreis@gmail.com>
2022-10-03 21:57:38 +0100

    Initial commit to set up react app and rename app to plants app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello and welcome to my plants app! 🌱
         </p>
         <a
           className="App-link"
