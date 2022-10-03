2f0896b2bd537ae887079f3ff0964d9c339a5ad8
Penelope Jungreis <penelope.jungreis@gmail.com>
2022-10-04 00:44:03 +0100

    listed countries and species count on frontend

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import places from './places.json'
 
 function App() {
 
-  console.log(places)
-  
+  console.log('I know my data is available in the browser because it is here!: ', places)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello and welcome to my plants app! 🌱
+          Hello and welcome to my plants app! 
         </p>
+        <ul>
+          {places.map(place => {
+            if (place.species_count > 0) {
+              return (
+                <li className="box" key={place.id
+                }>
+                  <h3>{place.name}</h3>
+                  <p>Species Count: {place.species_count}</p>
+                </li>
+              )
+            } else {
+              return ''
+            }
+          })}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
